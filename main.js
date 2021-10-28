d3342cbf94ebba50fea66224f2e6a1471d702bc4
Manuel Dias <54215983+Manuel8Dias@users.noreply.github.com>
2021-10-28 17:40:51 +0100

    Commit inicio aula2

---
+++ main.js
@@ -0,0 +1,18 @@
+
+
+const linksSocialMedia = {
+    github: "Manuel8Dias",
+    youtube: "UCc6E_MZ4L-L8fltEPzPM7Aw",
+    facebook: "ManuelDias.Dev",
+    instagram: "manueldias.dev",
+    twitter: "ManuelDias_VFX"
+}
+
+function changeSocialMedia() {
+    
+    for(let li of socialLinks.children) {
+        const social = li.getAttribute('class ')
+    }
+}
+
+// changeSocialMedia()
